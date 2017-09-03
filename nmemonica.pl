bf09647bf898ff458e379d1833e605a5b388e738
Bangsplat <theron@dankerton.net>
2017-09-02 20:23:52 -0700

    first working version

--- nmemonica.pl
+++ nmemonica.pl
@@ -3,10 +3,16 @@
 use strict;
 use warnings;
 
+my $stack_file_name = "stack.txt";
+my $stack_file_size = 1000;
 
+my $stack;
 
+open( my $fh, "<", $stack_file_name )
+    or die "Can't open stack.txt: $!";
+    
+read( $fh, $stack, $stack_file_size );
 
+print( "$stack\n" );
 
-
-
-
+close( $fh );
